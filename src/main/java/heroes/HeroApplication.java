ffca3a7110920bddbad4aabd76ce398b75dbd880
leonbogumilwps <128587998+leonbogumilwps@users.noreply.github.com>
2023-04-25 16:56:37 +0200

    Ändert CORS Einstellungen, damit DELETE und PUT funktionieren
    Moin Fred, bei dem "Tour of Heroe"s Tutorial mit deinem Java-Backend habe ich CORS-Errors bekommen: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS/Errors/CORSMissingAllowOrigin
    Mit dieser Änderung funktioniert bei mir das DELETE und das PUT wieder.

--- src/main/java/heroes/HeroApplication.java
+++ src/main/java/heroes/HeroApplication.java
@@ -17,7 +17,9 @@ public class HeroApplication {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+                registry.addMapping("/**").allowedOrigins("http://localhost:4200")
+                .allowedMethods("GET", "PUT", "POST", "DELETE")
+                .allowedHeaders("*");
             }
         };
     }
